6e23e3517d4bc0e1cfcb34db19008c70d14abcc1
Atul Acharya <adiatul2001@gmail.com>
2025-03-11 11:37:19 +0800

    Maze Generation Code

--- generator/maze/maze_generator.py
+++ generator/maze/maze_generator.py
@@ -1,25 +1,71 @@
-from maze_dataset import MazeDataset, MazeDatasetConfig
+import argparse
+import numpy as np
+import os
+import random
+from tqdm import tqdm
+
+from maze_dataset import MazeDataset, MazeDatasetConfig, SolvedMaze
 from maze_dataset.generation import LatticeMazeGenerators
 
 
-def generate_dataset(n_mazes, grid_n, directory):
-    # generate dataset according to parameters
-    # generate around 100_000? mazes
-    return
+def get_required_maze(dataset: MazeDataset, path_length: int):
+    return list(filter(lambda maze: len(maze.solution) == path_length, dataset))
+
+def generate_dataset(n_mazes, directory):
+    maze_dim = 5
+    
+    algorithms = {
+        "gen_dfs": LatticeMazeGenerators.gen_dfs,
+        "gen_wilson": LatticeMazeGenerators.gen_wilson,
+        "gen_prim": LatticeMazeGenerators.gen_prim
+    }
+
+    idx = 0
+    min_path_length = 2
+    max_path_length = 40
+    path_length = min_path_length
+    seed = 0
+
+    os.makedirs(directory, exist_ok=True)
+
+    with tqdm(total=n_mazes, desc="Generating Mazes", unit="maze") as pbar:
+        while idx < n_mazes:
+            seed += 1
+            
+            algorithm_name, algorithm = random.choice(list(algorithms.items()))
+
+            cfg: MazeDatasetConfig = MazeDatasetConfig(
+                seed=seed,
+                name="train", # name is only for you to keep track of things
+                grid_n=maze_dim, # number of rows/columns in the lattice
+                n_mazes=10000, # number of mazes to generate
+                maze_ctor=algorithm # algorithm to generate the maze
+            )
+
+            dataset: MazeDataset = MazeDataset.from_config(cfg)
+
+            required_mazes = get_required_maze(dataset, path_length)
+
+            print(len(required_mazes))
+
+            for maze in required_mazes:
+                maze_pixels = maze.as_pixels()  # Shape: (height, width, 3), dtype=uint8
+
+                maze_filename = os.path.join(directory, f"maze_{idx}.npy")
+                np.save(maze_filename, maze_pixels)
+
+                idx += 1
+
+            path_length = (path_length + 1) % (max_path_length + 1)
+            if path_length < min_path_length:
+                path_length = min_path_length
+
+            pbar.update(len(required_mazes))
 
 
 def main():
-    cfg: MazeDatasetConfig = MazeDatasetConfig(
-        name="train", # name is only for you to keep track of things
-        grid_n=5, # number of rows/columns in the lattice
-        n_mazes=4, # number of mazes to generate
-        maze_ctor=LatticeMazeGenerators.gen_dfs, # algorithm to generate the maze
-        maze_ctor_kwargs=dict(do_forks=False), # additional parameters to pass to the maze generation algorithm
-    )
-
-    dataset: MazeDataset = MazeDataset.from_config(cfg)
-
-    print(dataset[0].as_ascii())
+    # TODO: make this configurable  
+    generate_dataset(100_000, "./data/")
 
 if __name__ == '__main__':
     main()
